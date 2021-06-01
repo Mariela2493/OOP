1e403895e2dbb0c61f51f987530be7ab30fbbb72
Mariela2493 <mariela.sanchez.fallas@ucreativa.com>
2021-05-31 20:50:40 -0600

    Trabajo de Clase #7 31 may

--- src/com/ucreativa/vacunacion/Main.java
+++ src/com/ucreativa/vacunacion/Main.java
@@ -1,7 +1,7 @@
-package com.ucreativa;
+package com.ucreativa.vacunacion;
+
 import com.ucreativa.vacunacion.entities.BitacoraVacunacion;
 import com.ucreativa.vacunacion.entities.Familiar;
-import com.ucreativa.vacunacion.entities.Amigo;
 
 import java.util.ArrayList;
 import java.util.Date;
@@ -11,7 +11,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-     Familiar  mama = new Familiar ("Marjorie","1234", 55, true, "Madre");
+     Familiar mama = new Familiar ("Marjorie","1234", 55, true, "Madre");
      Familiar  papa = new Familiar ("Jorge","1234", 59, false, "Padre");
 
 
