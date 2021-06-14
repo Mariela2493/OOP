2e89ed3b5f3281a2ee2336d8a781a72352df395a
Mariela2493 <mariela.sanchez.fallas@ucreativa.com>
2021-06-14 17:54:25 -0600

    TRabajo de Clase # 10  10 Jun

--- src/com/ucreativa/vacunacion/Main.java
+++ src/com/ucreativa/vacunacion/Main.java
@@ -1,65 +1,54 @@
 package com.ucreativa.vacunacion;
-
 import com.ucreativa.vacunacion.entities.Amigo;
-import com.ucreativa.vacunacion.entities.BitacoraVacunacion;
 import com.ucreativa.vacunacion.entities.Familiar;
 import com.ucreativa.vacunacion.entities.Persona;
 import com.ucreativa.vacunacion.repositories.InMemoryRepository;
-import com.ucreativa.vacunacion.repositories.Repository;
-import com.ucreativa.vacunacion.repositories.FileRepository;
-
-import java.util.ArrayList;
+import com.ucreativa.vacunacion.services.BitacoraService;
 import java.util.Date;
-import java.util.List;
 import java.util.Scanner;
 
 public class Main {
 
 
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-
-        Repository repo= new InMemoryRepository();
-        String nombre, cedula, edad, riesgo, isAmigo, relacion, facebook, parentesco, marca, print;
-        Persona persona;
-        while (true) {
-            System.out.println("Nombre: ");
-            nombre= in.nextLine();
-            System.out.println("Cedula: ");
-            cedula= in.nextLine();
-            System.out.println("edad ");
-            edad= in.nextLine();
-            System.out.println("riesgo: ");
-            riesgo= in.nextLine();
-            System.out.println("Es amigo? : ");
-            isAmigo= in.nextLine();
-            if(isAmigo.equals("Si")){
-                System.out.println("Relacion: ");
-                relacion= in.nextLine();
-                System.out.println("Facebook: ");
-                facebook= in.nextLine();
-                persona = new Amigo(nombre,cedula,Integer.parseInt(edad),riesgo.equals("S"),relacion,facebook);
-            }else{
-
-                System.out.println("Parentesco");
-                parentesco= in.nextLine();
-                persona =new Familiar(nombre,cedula,Integer.parseInt(edad),riesgo.equals("S"),parentesco);
-
+        BitacoraService service = new BitacoraService(new InMemoryRepository());
+        String nombre, cedula, edad, riesgo, isAmigo, relacion = "",
+                facebook = "", parentesco = "", marca, print;
+        boolean seguir = true;
+        while (seguir) {
+            System.out.println("Nombre:");
+            nombre = in.nextLine();
+            System.out.println("Cedula:");
+            cedula = in.nextLine();
+            System.out.println("Edad:");
+            edad = in.nextLine();
+            System.out.println("Riesgo(S/N):");
+            riesgo = in.nextLine();
+            System.out.println("Amigo(A)/Familiar(F):");
+            isAmigo = in.nextLine();
+            if (isAmigo.equals("A")) {
+                System.out.println("Relacion:");
+                relacion = in.nextLine();
+                System.out.println("Facebook:");
+                facebook = in.nextLine();
+            } else {
+                System.out.println("Parentesco:");
+                parentesco = in.nextLine();
             }
-
-            System.out.println("Marca Vacuna: ");
-            marca= in.nextLine();
-            repo.save(persona, marca,new Date());
-            System.out.println("Quiere Limpiar La lista");
-            print= in.nextLine();
-            if(print.equals("Si")){
-                    for (String item : repo.get()){
-                        System.out.println(item);
-                    }
+            System.out.println("Vacuna -- Marca:");
+            marca = in.nextLine();
+            service.save(nombre, cedula, edad, riesgo, isAmigo, relacion,
+                    facebook, parentesco, marca);
+            System.out.println("Quiere imprimir Lista (S)");
+            print = in.nextLine();
+            if (print.equals("S")) {
+                for (String item : service.get()) {
+                    System.out.println(item);
                 }
             }
+            System.out.println("Continuar? (Default 'S')");
+            seguir = !in.nextLine().equals("N");
         }
-
     }
-
-
+}
--- src/com/ucreativa/vacunacion/repositories/InMemoryRepository.java
+++ src/com/ucreativa/vacunacion/repositories/InMemoryRepository.java
@@ -1,7 +1,6 @@
 package com.ucreativa.vacunacion.repositories;
-import com.ucreativa.vacunacion.entities.Amigo;
+
 import com.ucreativa.vacunacion.entities.BitacoraVacunacion;
-import com.ucreativa.vacunacion.entities.Familiar;
 import com.ucreativa.vacunacion.entities.Persona;
 
 import java.text.SimpleDateFormat;
@@ -9,24 +8,28 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+public class InMemoryRepository implements Repository {
 
-public class InMemoryRepository  implements Repository{
-  private  final List<BitacoraVacunacion> db;
+    private final List<BitacoraVacunacion> db;
 
-    public InMemoryRepository() {
-        this.db = new ArrayList <>();
+    public InMemoryRepository(){
+        this.db = new ArrayList<>();
     }
+
     public void save(Persona persona, String marca, Date fecha){
-        this.db.add(new BitacoraVacunacion(persona,marca,fecha));
+        this.db.add(new BitacoraVacunacion(persona, marca, fecha));
     }
-    public List<String> get(){
-        List <String> lines= new ArrayList<>();
-        SimpleDateFormat format= new SimpleDateFormat("dd/mm/yyyy");
-        for(BitacoraVacunacion item:db) {
 
-            lines.add(item.getPersona().getNombre()+ " " + item.getFecha()+ " " +item.getMarca());
-
+    public List<String> get() {
+        List<String> lines = new ArrayList<>();
+        SimpleDateFormat format = new SimpleDateFormat("dd-MM-yyyy hh:mm:ss");
+        for (BitacoraVacunacion item : db){
+            lines.add(item.getPersona().getNombre()
+                    + " - " + item.getMarca()
+                    + " - " + format.format(item.getFecha()));
         }
         return lines;
     }
+
+
 }
